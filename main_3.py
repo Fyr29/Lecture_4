2df2fce963e27af95bb82825bfbe06385a894534
Фыр <danil.erlih@gmail.com>
2026-03-11 21:40:41 +0800

    Home Work 3

--- main_3.py
+++ main_3.py
@@ -1,13 +1,9 @@
 import random
 
 amount = (random.randint(3, 10))
-spisok = []
 
-for i in range(amount):
-    spisok.append(random.randint(0, 10))
-
-print(spisok)
+spisok = [random.randint(0, 10) for i in range(amount)]
 
 spisok_2 = [spisok[0], spisok[2], spisok[-2]]
 
-print(spisok_2)
+print(f'{spisok} => {spisok_2}')
